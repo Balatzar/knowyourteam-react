4eb0938c4f0d583f1dc71c7f773b25fe2562754f
balatzar <bataprod@gmail.com>
2020-03-03 18:15:52 +0100

    Add links and fix footer

--- src/base/Footer.js
+++ src/base/Footer.js
@@ -1,31 +1,44 @@
 import React from "react";
 
+function Link({ link, text }) {
+  return (
+    <a
+      className="font-black underline text-purple-700 w-1/4 text-center"
+      href={link}
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      {text}
+    </a>
+  );
+}
+
 export default function Footer() {
   return (
-    <div className="fixed bottom-0 left-0 m-6 flex">
-      <span className="border-black border-4 border-solid p-1 flex-no-wrap uppercase font-black text-2xl">
-        Know Your Team
-      </span>
-      <span className="text-light text-2xl ml-4">|</span>
-      <span className="font-black uppercase underline text-2xl ml-4 text-purple-700">
-        <a
-          href="https://github.com/Balatzar"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          by
-        </a>
-      </span>
-      <span className="text-light text-2xl ml-4">|</span>
-      <span className="font-black uppercase underline text-2xl ml-4 text-purple-700">
-        <a
-          href="https://github.com/Balatzar/knowyourteam"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          code
-        </a>
-      </span>
+    <div className="fixed bottom-0 left-0 m-6">
+      <div className="flex justify-center">
+        <span className="border-black border-4 border-solid p-1 uppercase font-black text-2xl">
+          Know Your Team
+        </span>
+      </div>
+      <div className="flex justify-around">
+        <Link
+          text="made by balthazar aubard"
+          link="https://github.com/Balatzar"
+        />
+        <Link
+          text="see the code"
+          link="https://github.com/Balatzar/knowyourteam"
+        />
+        <Link
+          text="submit a bug"
+          link="https://airtable.com/tblJquHUujO27Yrt1/viw5sp6PXcD2Zix6o?blocks=hide"
+        />
+        <Link
+          text="request a feature"
+          link="https://airtable.com/tblJOwOUvaf660VI1/viwmP5B0TwcQ4rGOl?blocks=hide"
+        />
+      </div>
     </div>
   );
 }
